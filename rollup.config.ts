dff1bc003c9af26455a554d69b7c4173b342f68a
keli <15302737418@163.com>
2022-07-22 16:49:10 +0800

    fix: 添加commit提交规范校验配置

--- rollup.config.ts
+++ rollup.config.ts
@@ -3,6 +3,7 @@ import babel from 'rollup-plugin-babel';
 import { uglify } from 'rollup-plugin-uglify';
 import { eslint } from "rollup-plugin-eslint";
 import multiInput from 'rollup-plugin-multi-input';
+import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: ['src/**/*.ts'],
@@ -12,6 +13,9 @@ export default {
   },
   plugins: [
     multiInput({ relative: 'src/' }),
+    resolve({
+      extensions: ['.ts'],
+    }),
     eslint({
       fix: true,
       exclude: 'node_modules/**'
